6f8b96da761e3f07cf30e22a6c59f6643950e80a
Daniel Ortiz <DOrtiz1011@gmail.com>
2016-05-31 17:36:12 -0400

    Completed problem 1

--- KeyCounts.cs
+++ KeyCounts.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace DTHW
 {
     class KeyCounts
     {
+        private Dictionary<string, int> CountsByName { get; set; }
+        private string InputFile { get; set; }
+
+        public KeyCounts(string inputFile)
+        {
+            InputFile = inputFile;
+
+            if (CountsByName != null)
+            {
+                CountsByName.Clear();
+                CountsByName = null;
+            }
+
+            CountsByName = new Dictionary<string, int>();
+
+            ReadInputFile();
+        }
+
+        private void ReadInputFile()
+        {
+            foreach (var word in File.ReadAllLines(InputFile).ToList())
+            {
+                var inputs = word.Split(',');
+                var name = inputs[0];
+                var value = Convert.ToInt32(inputs[1]);
+
+                AddToDictionary(name, value);
+            }
+        }
+
+        private void AddToDictionary(string name, int value)
+        {
+            if (CountsByName.ContainsKey(name))
+            {
+                CountsByName[name] += value;
+            }
+            else
+            {
+                CountsByName.Add(name, value);
+            }
+        }
+
+        public void PrintResults()
+        {
+            const string format = "The total for {0} is {1}";
+            var stringBuiler = new StringBuilder();
+
+            foreach (var keyValuePair in CountsByName)
+            {
+                stringBuiler.AppendLine(string.Format(format, keyValuePair.Key, keyValuePair.Value));
+            }
+
+            Console.WriteLine(stringBuiler.ToString());
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace DTHW
+﻿using System;
+
+namespace DTHW
 {
     class Program
     {
         static void Main()
         {
+            var keyCounts = new KeyCounts("input.txt");
+            keyCounts.PrintResults();
 
+            Console.ReadLine();
         }
     }
 }
